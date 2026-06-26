86fe371d2c04b5209465c9c6ab3a9fbe220e00e1
ben-sentenac <bsr-web-09@proton.me>
2026-06-26 15:00:07 +0200

    feat: add bash integration entrypoint

---
+++ pixelforge.sh
@@ -0,0 +1,15 @@
+export PIXELFORGE_ROOT="${PIXELFORGE_ROOT:-$HOME/Workspace/projects/pixelForge}"
+
+source "$PIXELFORGE_ROOT/src/core/engine.sh"
+
+source "$PIXELFORGE_ROOT/src/providers/user.sh"
+source "$PIXELFORGE_ROOT/src/providers/host.sh"
+source "$PIXELFORGE_ROOT/src/providers/cwd.sh"
+
+source "$PIXELFORGE_ROOT/src/widgets/user.sh"
+source "$PIXELFORGE_ROOT/src/widgets/host.sh"
+source "$PIXELFORGE_ROOT/src/widgets/cwd.sh"
+
+source "$PIXELFORGE_ROOT/src/core/prompt.sh"
+
+pf_prompt_update
