4149af1375682519ae29afd3c8f96c5e4b472ce4
ben-sentenac <bsr-web-09@proton.me>
2026-06-27 15:03:46 +0200

    feat(ansi): complete ANSI color engine

---
+++ src/ansi/index.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source "${PIXELFORGE_ROOT}/src/ansi/colors.sh"
+source "${PIXELFORGE_ROOT}/src/ansi/styles.sh"
--- src/ansi/styles.sh
+++ src/ansi/styles.sh
@@ -2,5 +2,11 @@
 
 PF_ANSI_BOLD='\[\e[1m\]'
 PF_ANSI_DIM='\[\e[2m\]'
+PF_ANSI_ITALIC='\[\e[3m\]'
 PF_ANSI_UNDERLINE='\[\e[4m\]'
+PF_ANSI_REVERSE='\[\e[7m\]'
+
 PF_ANSI_NORMAL='\[\e[22m\]'
+PF_ANSI_NO_ITALIC='\[\e[23m\]'
+PF_ANSI_NO_UNDERLINE='\[\e[24m\]'
+PF_ANSI_NO_REVERSE='\[\e[27m\]'
